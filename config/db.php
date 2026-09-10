6a0756dd95deeb2209d841de007b0b8306d892e1
Ali <ali@gmail.com>
2026-09-10 15:54:46 +0500

    Initial Yii2 setup

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=padel_club',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
